cfac079674fe85e8f26b3558b90f6542064ce078
Bojan Stefanovic <bojanstef4@gmail.com>
2016-04-29 18:14:05 -0700

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const _ = require('underscore');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -29,18 +30,15 @@ app.get('/', function(request, response) {
 	response.send('Todo: API Root');
 });
 
+// GET /todos
 app.get('/todos', function(request, response) {
 	response.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function(request, response) {	
-	const todoId = parseInt(request.params.id, 10); // 2nd argument is the Base
-	var matchedTodo;
-	todos.forEach(function(todo) {
-		if (todo.id === todoId) {
-			matchedTodo = todo;
-		}
-	});
+	const todoId = parseInt(request.params.id, 10); // 2nd argument is the Base	
+	const matchedTodo = _.findWhere(todos, {id: todoId});	
 	if (matchedTodo) {
 		response.json(matchedTodo);
 	}
@@ -49,9 +47,17 @@ app.get('/todos/:id', function(request, response) {
 	}
 });
 
-// POST Request
+// POST /todos 
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed'); // Use only desc and completed fields.
+
+	// IF body is not completed OR body description is not a string OR body description is empty, return an error.
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
+
+	// Remove leading and trailing whitespace
+	body.description = body.description.trim();
 
 	body.id = todoNextId++;
 	todos.push(body);
