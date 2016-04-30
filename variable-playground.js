0e74e388e14573c19b208df2c5e03f28d0e4b746
Bojan Stefanovic <bojanstef4@gmail.com>
2016-04-29 22:16:11 -0700

    Add var playground.js

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name: "bojan",
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	obj = {
+// 		name: "bojan",
+// 		age: 24
+// 	};
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+grades = [24, 24, 35, 47, 24, 36, 42];
+
+function updateArrayPush(arr) {
+	grades.push(20);
+	debugger; // breakpoint
+}
+updateArrayPush(grades);
+console.log(grades);
